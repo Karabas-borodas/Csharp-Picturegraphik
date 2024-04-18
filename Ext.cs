6ecdd67e9e4038ef177f4ed1766b0e827d5e06c7
Korobas-borodas <zoood3.14@gmail.com>
2024-04-18 23:04:44 +0300

    Добавьте файлы проекта.

---
+++ Ext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Picturegraphik
+{
+    public static class Ext
+    {
+        public static void TogRaseScale(this Bitmap bitmap)
+        {
+            for (int y = 0; y <bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    int Avg = (pixel.R + pixel.B + pixel.G) / 3;
+                    bitmap.SetPixel(x, y, Color.FromArgb (pixel.A, Avg, Avg, Avg));
+                }
+
+            }
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Csharp_Picturegraphik
+{
+    class Program
+    {
+        private const double Widse = 1.5;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG "
+            };
+            Console.WriteLine(" Pleas Press ENTER");
+            while (true)
+            {
+                Console.ReadLine();
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+                Console.Clear();
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = Resize(bitmap);
+                bitmap.TogRaseScale();
+
+                var convert = new Converter(bitmap);
+                var rows = convert.Convert();
+
+                foreach (var row in rows)
+                {
+                    Console.Write(row);
+
+                }
+                Console.SetCursorPosition(0,0);
+
+               
+            }
+        }
+        private static Bitmap Resize(Bitmap bitmap)
+        {
+            var MaxWidth = 350;
+            var NewHight = bitmap.Height / Widse * MaxWidth / bitmap.Width;
+            if (bitmap.Width > MaxWidth || bitmap.Height > NewHight)
+                bitmap = new Bitmap(bitmap, new Size(MaxWidth, (int)NewHight));
+            return bitmap;
+        }
+    }
+}
